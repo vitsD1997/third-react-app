f3e63cead2ef102195d587d027ce4caf1edac77b
vitthal <dhakanevitthal30@gmail.com>
2023-06-16 00:15:14 +0530

    hello message

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 function App() {
   return (
     <>
-    <h1>hello demo</h1>
-    <MessageDemo/>
+    
+    <MessageDemo name="vitthal"/>
+    <MessageDemo name="amol"/>
+    <MessageDemo name="babasaheb"/>
+    <MessageDemo name="rghunathrao"/>
     </>
   );
     
   
 }
 
-function MessageDemo() {
+function MessageDemo(props) {
   return(
     <>
     <h1>Hello Message</h1>
