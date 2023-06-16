9c003cd82950eefd6e829757a29225d50a7f9107
vitthal <dhakanevitthal30@gmail.com>
2023-06-16 10:54:58 +0530

    use-setlist

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { useState } from "react";
+
+
 function App() {
   return (
     <>
-    <h1>props demo</h1>
+    <h1>Stateful demo</h1>
     <ListDemo />
     </>
   );
@@ -10,28 +13,24 @@ function App() {
 }
 
 function ListDemo() {
-  let data = "hello universe";
-  let list = ["", "", "", "", ""];
+ let [list, setList] = useState(["delhi"]);
+ 
+  let addItemAction = () => {
+    let newList = [...list, "mumbai"];
 
-  let list1 = [];
-  for (let i = 0; i < 100; i++){
-    list1.push("");
-  }
+   console.log(newList);
+
+   setList(newList);
+  };
   return(
     <>
-
-      <h1>{data}</h1>
+      <input type="button" value="add new item" onClick={addItemAction} />
+      <h1>data</h1>
       {list.map((item) => (
         <>
-        <h1>hello world</h1>
+        <h1>hello {item}</h1>
         </>
       ))}
-
-     {list1.map((item) =>(
-      <div>
-        <h1>hello universe</h1>
-      </div>
-     ))}
     </>
   );
 }
